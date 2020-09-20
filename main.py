e1928232a7250be2625e0a62b20550dcdea7f3a9
George <71167099+letow@users.noreply.github.com>
2020-09-20 19:42:55 +0500

    Added basic encryption functionality
    At the moment scrambler gives a choice of mode (encrypt/decrypt), gets a string from user and returns encrypted/decrypted message.
    Works with only english letters and spaces right now.

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-def get_key(val): 
-  for key, value in dic.items(): 
-    if val == value: 
-      return key 
-  return " "
-
-def encrypt(input("Enter message: "))
-
 dic = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25, ' ':100}
-word = "zoopark yota"
-new_word = []
-step = 3
+def get_key(val):
+	for key, value in dic.items():
+		if val == value:
+			return key
+	return " "
+def caesar(mode, word, step):
+	new_word = []
+	for i in list(word):
+		new_word.append(dic.get(i))
+	new_word_word = []
+	if mode == 1:
+		for j in new_word:
+			if j+step > 25:
+				new_word_word.append(get_key(j+step-26))
+			else:
+				new_word_word.append(get_key(j+step))
+	elif mode == 2:
+		for j in new_word:
+			if j - step < 0:
+				new_word_word.append(get_key(j-step + 26))
+			else:
+				new_word_word.append(get_key(j-step))
 
-for i in list(word):
-  new_word.append(dic.get(i))
+	return ''.join(new_word_word)
 
-new_word_word = []
-
-for j in new_word:
-  if j+step > 25:
-    new_word_word.append(get_key(j+step-26))
-  else:
-    new_word_word.append(get_key(j+step))
-print(''.join(new_word_word))
+print('Welcome to the Caesar Cipher!\nModes:\n1 - Encrypt message\n2 - Decrypt message')
+print(caesar(int(input('Choose mode: ')), str(input('Enter message: ')), int(input('Enter step: '))))
